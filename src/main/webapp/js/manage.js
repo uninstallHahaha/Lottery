d8ec943d0b7afd1ac15f3705d342a49fb5b02785
uninstallHahaha <2234261505@qq.com>
2020-04-16 14:30:08 +0800

    vue时间格式化问题解决

--- src/main/webapp/js/manage.js
+++ src/main/webapp/js/manage.js
@@ -1,5 +1,27 @@
 $(function () {
 
+    Vue.filter('dataFormat', function (value, fmt) {
+        let getDate = new Date(value);
+        let o = {
+            'M+': getDate.getMonth() + 1,
+            'd+': getDate.getDate(),
+            'h+': getDate.getHours(),
+            'm+': getDate.getMinutes(),
+            's+': getDate.getSeconds(),
+            'q+': Math.floor((getDate.getMonth() + 3) / 3),
+            'S': getDate.getMilliseconds()
+        };
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (getDate.getFullYear() + '').substr(4 - RegExp.$1.length))
+        }
+        for (let k in o) {
+            if (new RegExp('(' + k + ')').test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+            }
+        }
+        return fmt;
+    });
+
     function initUserInfo() {
         $.ajax({
             url: 'getLoginUserInfo',
@@ -25,8 +47,8 @@ $(function () {
             url: 'getUsers',
             method: 'POST',
             success: (res) => {
-                res = res.filter(function(item) {
-                         return item.id != app.$data.userData.id
+                res = res.filter(function (item) {
+                    return item.id != app.$data.userData.id
                 });
                 app.$data.usersList = res
             }
@@ -58,15 +80,32 @@ $(function () {
             initNews()
             initUsers()
         },
+        computed: {
+            formatTime: function (datetime) {
+                var date = new Date(datetime);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+                var year = date.getFullYear(),
+                    month = ("0" + (date.getMonth() + 1)).slice(-2),
+                    sdate = ("0" + date.getDate()).slice(-2),
+                    hour = ("0" + date.getHours()).slice(-2),
+                    minute = ("0" + date.getMinutes()).slice(-2),
+                    second = ("0" + date.getSeconds()).slice(-2);
+                // 拼接
+                var result = year + "-" + month + "-" + sdate + " " + hour + ":" + minute + ":" + second;
+                // 返回
+                return result;
+            }
+        },
         methods: {
             //切换到发布
             switchPage: function () {
                 this.ifmain = false
-            },
+            }
+            ,
             //返回到管理台
             returnMain: function () {
                 this.ifmain = true
-            },
+            }
+            ,
             //发布新闻
             sendArticle: function () {
                 $.ajax({
@@ -81,7 +120,8 @@ $(function () {
                         }
                     }
                 })
-            },
+            }
+            ,
             //草稿
             saveArticle: function () {
                 $.ajax({
@@ -96,7 +136,8 @@ $(function () {
                         }
                     }
                 })
-            },
+            }
+            ,
             //草稿箱
             showScrs: function () {
                 layer.open({
@@ -107,7 +148,8 @@ $(function () {
                     area: ['500px', '90%'],
                     content: 'getScrsPage'
                 });
-            },
+            }
+            ,
             //删除新闻
             delNews: function (id, title) {
                 layer.confirm('确定删除新闻 "' + title + '" ?', {
@@ -124,9 +166,10 @@ $(function () {
                         }
                     })
                 })
-            },
+            }
+            ,
             //注销用户
-            outUser: function (id,account) {
+            outUser: function (id, account) {
                 layer.confirm('确定注销用户 "' + account + '" ?', {
                     btn: ['确认', '取消']
                 }, function () {
@@ -141,15 +184,17 @@ $(function () {
                         }
                     })
                 })
-            },
+            }
+            ,
             //邮箱tip
-            tipEmail:function () {
+            tipEmail: function () {
                 layer.tips('绑定邮箱后可使用邮箱找回密码', '#bindEmail')
-            },
+            }
+            ,
             //绑定邮箱
             bindEmail: function () {
                 layer.open({
-                    title:'绑定邮箱',
+                    title: '绑定邮箱',
                     type: 2,
                     // btn: ['确定', '取消'],
                     area: ['420px', '240px'], //宽高
@@ -158,5 +203,7 @@ $(function () {
             }
         }
     })
+
+
 
 })
